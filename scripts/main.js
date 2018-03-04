f80052c18f03f9b2e8256e057f8545d119427547
Miguel de Moura <me@migueldemoura.com>
2018-03-04 17:07:59 +0000

    Temporary update for 2018 edition

--- scripts/main.js
+++ scripts/main.js
@@ -11,23 +11,4 @@ $(document).ready(function () {
             toggle.trigger('click')
         }
     });
-
-    $('.dropdown-menu li a').click(function(e) {
-        e.preventDefault();
-        $(this).parents('.dropdown').find('#selected-course').text($(this).text());
-        $(this).parents('.dropdown').find('.btn').val($(this).data('value'));
-    });
-
-    $('.filter-course').on('click', function () {
-        var $this = $(this);
-
-        $('.filter-course.active').removeClass('active');
-        $this.addClass('active');
-
-        var course = $this.data('filter');
-        $('.filterable-course').each(function () {
-            var $this = $(this);
-            $this.parent()[(course === 'All' || $this.text().indexOf(course) !== -1) ? 'show' : 'hide']();
-        });
-    });
 });
